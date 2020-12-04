027d9dbe6f91e13ac7e4eb2ba66a214161a15382
Albin <a.bramstang@gmail.com>
2020-12-04 22:22:03 +0100

    Day 4

---
+++ Day4/Program.fs
@@ -0,0 +1,79 @@
+﻿open System.IO
+open Validators
+
+let getLength (x : 'a list) = x.Length
+
+let splitPassports data =
+    let rec loop data current (all : string list) =
+        match data with
+        | [] -> current :: all
+        | head :: tail ->
+            match head with
+            | "" -> loop tail "" (current :: all)
+            | line -> loop tail (current + " " + line) all
+    let passports = loop data "" []
+    printfn "Total passports %i" passports.Length
+    passports
+
+let isValid (password : string) =
+    let checkPart (part : string) =
+        match part.[..2] with
+        | "byr" -> 1
+        | "iyr" -> 1
+        | "eyr" -> 1
+        | "hgt" -> 1
+        | "hcl" -> 1
+        | "ecl" -> 1
+        | "pid" -> 1
+        | _ -> 0
+    let parts = password.Split ' '
+    Seq.sum (Seq.map checkPart parts) = 7
+
+let countValidPassports data =
+    (Seq.toList (Seq.where (fun x -> x) (Seq.map (fun x -> isValid x ) data))).Length
+
+let isValid2 (password : string) =
+    let checkPart (part : string) =
+        match part.[..2] with
+        | "byr" -> isValidBirthYear part.[4..]
+        | "iyr" -> isValidIssueYear part.[4..]
+        | "eyr" -> isValidExpirationYear part.[4..]
+        | "hgt" -> isValidHeight part.[4..]
+        | "hcl" -> isValidHairColor part.[4..]
+        | "ecl" -> isValidEyeColor part.[4..]
+        | "pid" -> isValidPassportId part.[4..]
+        | _ -> false
+    let parts = password.Split ' '
+    let oks =
+        Seq.map checkPart parts |>
+            Seq.where (fun x -> x) |>
+                Seq.toList |>
+                    getLength
+    oks = 7
+
+let countValidPassports2 data =
+    Seq.map (fun x -> isValid2 x ) data |>
+        Seq.where (fun x -> x) |>
+            Seq.toList |>
+                getLength
+
+[<EntryPoint>]
+let main argv =
+    let input = Seq.toList (File.ReadAllLines "Input.txt")
+    let passports = splitPassports input
+    
+    // Part 1
+    printfn "Answer to part 1 is %i" (countValidPassports passports)
+
+    // Part 2
+    printfn "Answer to part 2 is %i" (countValidPassports2 passports)
+
+    //let valid = Seq.toList (File.ReadAllLines "Valid.txt")
+    //let validPassports = splitPassports valid
+    //printfn "Valid %i" (countValidPassports2 validPassports)
+
+    //let invalid = Seq.toList (File.ReadAllLines "Invalid.txt")
+    //let invalidPassports = splitPassports invalid
+    //printfn "Invalid %i" (countValidPassports2 invalidPassports)
+
+    0 // return an integer exit code
---
+++ Day4/Validators.fs
@@ -0,0 +1,68 @@
+﻿module Validators
+
+let safeParse i =
+    try
+        let value = i |> int
+        Some value
+    with
+        | _ -> None
+
+let isValidBirthYear year =
+    match safeParse year with
+    | Some y -> 1920 <= y && y <= 2002
+    | None -> false
+
+let isValidIssueYear year =
+    match safeParse year with
+    | Some y -> 2010 <= y && y <= 2020
+    | None -> false
+
+let isValidExpirationYear year =
+    match safeParse year with
+    | Some y -> 2020 <= y && y <= 2030
+    | None -> false
+
+let isValidHeight (height : string) =
+    if height.EndsWith "cm" then
+        let heightTrimmed = height.TrimEnd [| 'c'; 'm' |]
+        let maybeHeight = safeParse heightTrimmed
+        match maybeHeight with
+        | Some value -> 150 <= value && value <= 193
+        | None -> false
+    elif height.EndsWith "in" then
+        let heightTrimmed = height.TrimEnd [| 'i'; 'n' |]
+        let maybeHeight = safeParse heightTrimmed
+        match maybeHeight with
+        | Some value -> 59 <= value && value <= 76
+        | None -> false
+    else
+        false
+
+let isValidHairColor (color : string) =
+    if color.StartsWith "#" && color.Length = 7 then
+        let values = Seq.toList color.[1..]
+        let characters = List.append ['a'..'f'] ['0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9']
+        Seq.map (fun x -> (Seq.contains x characters)) values |>
+            Seq.contains false |>
+                not
+    else
+        false
+
+let isValidEyeColor color =
+    match color with
+    | "amb"
+    | "blu"
+    | "brn"
+    | "gry" 
+    | "grn"
+    | "hzl" 
+    | "oth" -> true
+    | _ -> false
+
+let isValidPassportId (passportId : string) =
+    if passportId.Length = 9 then
+        match safeParse passportId with
+        | Some _ -> true
+        | None -> false
+    else
+        false
